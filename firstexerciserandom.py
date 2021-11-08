0d498a11940b06f094c2334e386f8704ba098ad1
Zlata Zelenskaya <93883858+smolprogrammer@users.noreply.github.com>
2021-11-08 16:54:09 +0200

    Add files via upload
    first exercise, now works

---
+++ firstexerciserandom.py
@@ -0,0 +1,23 @@
+import random
+
+bars = ["Shoolbred's",
+        "The Wren",
+        "The Scratcher",
+        "ACME",
+        "Blind Barber"]
+
+people = ["Mattan",
+          "Chris",
+          "that person you forgot to text back",
+          "Kanye West",
+          "Samuel L. Jackson",
+          "Zlata"]
+
+random_first = random.choice(people)
+random_second = random.choice(people)
+while random_second == random_first:
+    random_second = random.choice(people)
+
+random_bar = random.choice(bars)
+
+print(f'How about you go to {random_bar} with {random_first} and {random_second}?')
