76d76f13d8115016ff49ce2bca95c6b3560d067f
Zlata Zelenskaya <93883858+smolprogrammer@users.noreply.github.com>
2021-11-09 18:55:05 +0200

    Add files via upload

---
+++ truefalsepractice.py
@@ -0,0 +1,20 @@
+True and True # true
+False and True # false
+1 == 1 and 2 == 1 # false
+"love" == "love" # true
+1 == 1 or 2 != 1 # true
+True and 1 == 1 # true
+False and 0 != 0 # false
+True or 1 == 1 # true
+"time" == "money" # false
+1 != 0 and 2 == 1 # false
+"I Can't Believe It's Not Butter!" != "butter" # true
+"one" == 1 # false
+not (True and False) # true
+not (1 == 1 and 0 != 1) # false
+not (10 == 1 or 1000 == 1000) # false
+not (1 != 10 or 3 == 4) # false
+not ("love" == "love" and "time" == "money") # true
+1 == 1 and (not ("one" == 1 or 1 == 0)) # true
+"chunky" == "bacon" and (not (3 == 4 or 3 == 3)) # false
+3 == 3 and (not ("love" == "love" or "Python" == "Fun")) # false
