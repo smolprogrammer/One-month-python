9138e7a35885a8e0abf9cf097e872bac591897dc
Zlata Zelenskaya <93883858+smolprogrammer@users.noreply.github.com>
2021-11-09 19:42:28 +0200

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print "Fizz" instead of the number
+# and for the multiples of five print "Buzz".
+# For numbers that are multiples of both three and five print "FizzBuzz".
+
+# Tip: % (modulo) tells you what's left over when you divide one number by another
+# ex. number % 3 == 0
+
+for number in range(1, 101):
+    if number % 5 == 0 and number % 3 == 0:
+        print('Fizzbuzz')
+    elif number % 5 == 0:
+        print('Buzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    else:
+        print(number)
